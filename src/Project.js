c6408c31fd3308bc204cf1e37525c0ec66ca1a12
jiajunliu0024 <jiajunliu0024@gmail.com>
2022-03-31 09:40:55 +1100

    add the ant design

--- src/Project.js
+++ src/Project.js
@@ -1,13 +1,70 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Button } from 'antd';
+import { Card } from 'antd';
+import {Row, Col} from 'antd';
 function ProjectContent(){
     
   
     return(
         
         <body className="project-page">
+            <div className="project-card-content">
+                <div className = "project-container-card" >
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+                <div className = "project-container-card" >
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+                <div className = "project-container-card" >
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                    <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </div>
+            
            
-            <div className = "card" >
+            {/* <div className = "card" >
                 <div className="card-design">
                     <p>React Project - Animation auto</p>
                     <div className = "card-presonal-info">
@@ -44,7 +101,7 @@ function ProjectContent(){
                 </div>
                 <img className = "card-img" src = "./logo192.png"></img>
                 
-            </div>
+            </div> */}
             
         
         </body>
@@ -54,6 +111,7 @@ function ProjectContent(){
 
 }
 function Project(){
+    
     return(
       <div>
         
