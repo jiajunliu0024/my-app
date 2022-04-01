1beba5900802c50284534e292e73cdf07c854574
jiajunliu0024 <jiajunliu0024@gmail.com>
2022-04-01 13:45:38 +1100

    handle adding card

--- src/Project.js
+++ src/Project.js
@@ -1,14 +1,56 @@
-import Header from "./Header";
+
 import Footer from "./Footer";
-import { Button } from 'antd';
 import { Card } from 'antd';
-import {Row, Col} from 'antd';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FilterCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count : 0}
+        this.handleAddingDivs = this.handleAddingDivs.bind(this)
+    }
+
+
+    handleAddingDivs(e) {
+        console.log(e.target.value);
+        this.setState({count:parseInt(e.target.value)});     
+    }
+    
+    renderDivs(){
+        let count = this.state.count, uiItems = [];
+        while(count--)
+           uiItems.push(
+            <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+            </Card>
+            )
+        return uiItems;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>These are added divs </h1>
+                <button className="btn-anchor-style add-row-link" value={"2"}  type="button" onClick={e => this.handleAddingDivs(e, "value")} >{'2'}</button>
+                {this.renderDivs()}
+            </div>
+        )
+    }
+}
+
 function ProjectContent(){
+
+
+
     
   
     return(
         
         <body className="project-page">
+            <FilterCard/ >
             <div className="project-card-content">
                 <div className = "project-container-card" >
                     <Card className = "project-card"  title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
